2f73bcb266bf02d94a961798ffb3c19cad018af4
nastialyash <user251@school22.rv.ua>
2024-09-04 21:24:48 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,50 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+	
+	
+	std::cout << "I made myself a snowball,\n"
+		"As perfect as could be.\n"
+		"I thought I'd keep it as a pet\n"
+		"And let it sleep with me\n"
+		"\n"
+		"\t\t\tfrom google\n";
+
+
+
+	
+
+	std::cout << "Every\n";
+	std::cout << "\tHunter\n";
+	std::cout << "\t\tWants\n";
+	std::cout << "\t\t\tTo\n";
+	std::cout << "\t\t\t\tKnow\n";
+	std::cout << "\t\t\t\t\tWhere\n";
+	std::cout << "\t\t\t\t\t\tPheasant\n";
+	std::cout << "\t\t\t\t\t\t\tSits\n";
+
+
+
+
+	std::cout << "Puppies for sale\n";
+	std::cout << "Dachshund: breed\n";
+	std::cout << "Age: 2 months\n";
+	std::cout << "Price: 300grn\n";
+	std::cout << "Num: 222-22-22\n";
+	std::cout << "222 | 222 |\n";
+	std::cout << "22  | 22  |\n";
+	std::cout << "22  | 22  |\n";
+
+
+ 
+
+
+
+
+
+
+}
+
